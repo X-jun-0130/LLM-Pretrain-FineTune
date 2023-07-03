868f2a87b4c1052961b2ddc71fc3926afb3a6480
No_GPT <46883256+X-jun-0130@users.noreply.github.com>
2023-07-03 19:23:44 +0800

    Update Model_Bloom_Sft.py
    使用datase多线程处理数据，加快时间，减少内存占用

--- Instructions_FineTune/Model_Bloom_Sft.py
+++ Instructions_FineTune/Model_Bloom_Sft.py
@@ -1,35 +1,21 @@
-# deepspeed --master_addr 172.16.0.126 --master_port 5050 --include localhost:0,1,2,3,4,5,6,7  ./Model_Bloom_Sft.py
+# deepspeed --master_addr 58.210.77.164 --master_port 60147 --include localhost:0,1,2,3,4,5,6,7  ./sft_model.py
 import os
 import re
-import copy
-
+import numpy as np
 import torch
 import json
+import random
+import copy
+from datasets import load_dataset
 from torch.utils.data import Dataset, random_split
 from transformers import AutoTokenizer, TrainingArguments, Trainer, AutoModelForCausalLM
 
 IGNORE_INDEX = -100
 
-model_name = "./Bloom_6b4_pretrain/"
+model_name = '/workspace/gptpretarin/'
 torch.manual_seed(42)
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-training_args = TrainingArguments(output_dir='./results', 
-                                 num_train_epochs=3, 
-                                 learning_rate=1e-6,
-                                 logging_steps=100, 
-                                 save_strategy='steps',
-                                 save_steps = 2500,
-                                 evaluation_strategy = 'epoch',
-                                 per_device_train_batch_size=32, 
-                                 per_device_eval_batch_size=32, 
-                                 lr_scheduler_type="cosine",
-                                 warmup_steps=30,
-                                 weight_decay=0.01,
-                                 fp16=True,
-                                 gradient_checkpointing=True,
-                                 deepspeed='./config_file/ds_config_sft.json')
-model = AutoModelForCausalLM.from_pretrained(model_name, use_cache =False).cuda()
 
 def tokenize(text):
     inputs_with_offsets = tokenizer(text, return_offsets_mapping=True)
@@ -51,11 +37,12 @@ def tokenize(text):
 
             if start_idx is not None and end_idx is not None:
                 idx.extend([j for j in range(start_idx, int(end_idx))])
+                
     idx = list(set(idx))
     if len(idx) > 0:
-        for k in range(len(labels)):
-            if k not in idx:
-                labels[k] = IGNORE_INDEX
+        not_idx = [k for k in range(len(labels)) if k not in idx]
+        for t in not_idx:
+            labels[t] = IGNORE_INDEX
     labels[-1] = 2
 
     return dict(
@@ -65,87 +52,41 @@ def tokenize(text):
     )
 
 '''
-1.缺少指标异常、正常的指令  qa   2000
-2.缺少多轮知识问答的数据（英翻中） dia  500
-
-3.医患对话生成报告   300
-4.书籍问答知识题   600（各类)
-5.诊断结论  400(各类)
-6.实体识别  300 
-7.报告摘要  600(各类)
-8.报告解读 500（各类）
-9.检验指标抽取分析 500（各类）
-10.阴阳性 (200)
-'''
-class data_sets(Dataset):
-    def __init__(self, txt_list):
-        self.input_ids = []
-        self.attn_masks = []
-        self.labels = []
-        for i, txt in enumerate(txt_list):
-            encodings_dict = tokenize(txt)
-            # padding_len = max(0, max_length - len(encodings_dict['input_ids']))
-            self.input_ids.append(encodings_dict['input_ids'])
-            self.attn_masks.append( encodings_dict['attention_mask'])
-            self.labels.append(encodings_dict['labels'])
-            print(i)
-    def __len__(self):
-        return len(self.input_ids)
-
-    def __getitem__(self, idx):
-        return self.input_ids[idx], self.attn_masks[idx], self.labels[idx]
-
-
-# class data_sets(Dataset):
-#     def __init__(self, txt_list,  max_length):
-#         self.input_ids = []
-#         self.attn_masks = []
-#         for txt in txt_list:
-#             encodings_dict = tokenizer(txt,  max_length=max_length, padding="max_length")
-#             self.input_ids.append(torch.tensor(encodings_dict['input_ids']))
-#             self.attn_masks.append(torch.tensor(encodings_dict['attention_mask']))
-
-#     def __len__(self):
-#         return len(self.input_ids)
-
-#     def __getitem__(self, idx):
-#         return self.input_ids[idx], self.attn_masks[idx]
-  
-'''
 data_process
 '''
-instructions = json.load(open('./Nlp_2023/Medical_data/instruction/all_instruction.json', 'r', encoding='utf-8'))
-print(len(instructions))
-print(instructions[100])
+instruction_dataset = load_dataset("json", data_files='/workspace/model_sft/instruction_2048.json', split="train")
+tokenized_dataset = instruction_dataset.shuffle().map(lambda x: tokenize(x["text"]), num_proc=32)
+print(len(tokenized_dataset))
 
-instruction_list = []
-for line in instructions:
-    k = tokenizer.encode(line)
-    if len(k) <= 1024:
-        instruction_list.append(line)
-
-print(len(instruction_list))
-
-dataset = data_sets(instruction_list)
-train_size = int(0.88 * len(dataset))
-train_dataset, val_dataset = random_split(dataset, [train_size, len(dataset) - train_size])
-print(len(instruction_list), len(train_dataset))
-#59168
-
+train_size = int(0.95 * len(tokenized_dataset))
+train_dataset, val_dataset = random_split(tokenized_dataset, [train_size, len(tokenized_dataset) - train_size])
+print(len(train_dataset), len(val_dataset))
 '''
 model training
 '''
-# def the_collate_fn(batch):  
-#     input_ids = torch.stack([f[0] for f in batch])
-#     attention_mask = torch.stack([f[1] for f in batch])
-#     return {'input_ids':input_ids, 'attention_mask':attention_mask, 'labels':input_ids}
-
+training_args = TrainingArguments(output_dir='./results', 
+                                 num_train_epochs=2, 
+                                 learning_rate=3e-6,
+                                 logging_steps=100, 
+                                 save_strategy='epoch',
+                                #  save_steps = 150,
+                                 evaluation_strategy = 'epoch',
+                                 per_device_train_batch_size=20, 
+                                 per_device_eval_batch_size=20, 
+                                 lr_scheduler_type="cosine",
+                                 warmup_steps= 3000,
+                                 weight_decay=0.01,
+                                 fp16=True,
+                                 gradient_checkpointing=True,
+                                 deepspeed='./config_file/config_sft.json')
+model = AutoModelForCausalLM.from_pretrained(model_name, use_cache =False).cuda()
+print('model_loaded')
 
 def the_collate_fn(batch):  
-    maxlength = max([len(f[0]) for f in batch])
-    input_ids = torch.stack([torch.tensor([3]*(maxlength-len(f[0])) + f[0]) for f in batch])
-    attention_mask = torch.stack([torch.tensor([0]*(maxlength-len(f[1])) + f[1]) for f in batch])
-    labels = torch.stack([torch.tensor([-100]*(maxlength-len(f[2])) + f[2]) for f in batch])
+    maxlength = max([len(f['input_ids']) for f in batch])
+    input_ids = torch.stack([torch.tensor([3]*(maxlength-len(f['input_ids'])) + f['input_ids']) for f in batch])
+    attention_mask = torch.stack([torch.tensor([0]*(maxlength-len(f['attention_mask'])) + f['attention_mask']) for f in batch])
+    labels = torch.stack([torch.tensor([-100]*(maxlength-len(f['labels'])) + f['labels']) for f in batch])
     return {'input_ids':input_ids, 'attention_mask':attention_mask, 'labels':labels}
 
 
