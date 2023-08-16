bac7cdbae7330ceb8a78d9c60e964d1c734ee648
No_GPT <46883256+X-jun-0130@users.noreply.github.com>
2023-08-16 22:28:57 +0800

    使用flashattention-2加速微调，显存占用大幅减少

--- Instructions_FineTune/ChineseLlama2_Sft.py
+++ Instructions_FineTune/ChineseLlama2_Sft.py
@@ -1,18 +1,22 @@
-# deepspeed --master_addr xxx.xxx.xxx.xxx --master_port 5050 --include localhost:0,1,2,3,4,5,6,7  ./ChineseLlama2_Sft.py
-import re
+# deepspeed --master_addr 172.xx.xx --master_port 5050 --include localhost:0,1,2,3,4,5,6,7  ./Model_Bloom_Sft.py
 import torch
 import copy
 from datasets import load_dataset
 from torch.utils.data import random_split
 from transformers import AutoTokenizer, TrainingArguments, Trainer, AutoModelForCausalLM,LlamaTokenizer,LlamaForCausalLM
-import logging
-logging.disable(30)
+from transformers.optimization import get_cosine_schedule_with_warmup
+import warnings
+warnings.filterwarnings("ignore")
 
 IGNORE_INDEX = -100
 flash_attn = True
 
-model_name = '/Model_TH/Chinese-LLaMA2-7B/'
+model_name = '/workspace/Chinese_llama2_7B/epoch3/'
 torch.manual_seed(42)
+
+if flash_attn:
+    from flash_attn_patch import replace_llama_attn_with_flash_attn
+    replace_llama_attn_with_flash_attn()
 
 tokenizer = LlamaTokenizer.from_pretrained(model_name)
 
@@ -21,7 +25,7 @@ print([tokenizer.decode(START_IDS)])
 END_IDS = 2
 
 def tokenize(text):
-    inputs_with_mask = tokenizer(text)
+    inputs_with_mask = tokenizer(text['text'])
     inputs = inputs_with_mask['input_ids']
     labels = [-100] * len(inputs)
     
@@ -40,39 +44,41 @@ def tokenize(text):
 '''
 data_process
 '''
-instruction_dataset = load_dataset("json", data_files='Nlp_2023/Medical_data/instruction/all_instruction_chinesellama.json', split="train")
-tokenized_dataset = instruction_dataset.shuffle().map(lambda x: tokenize(x["text"]), num_proc=64)
+instruction_dataset = load_dataset("json", data_files='/workspace/Nlp_2023/Medical_data/mix_instruction/all_instruction_Chinesellama.json', split="train")
+tokenized_dataset = instruction_dataset.map(tokenize, remove_columns=instruction_dataset.column_names, num_proc=64, keep_in_memory=False)
 print(len(tokenized_dataset))
 
-train_size = int(0.98 * len(tokenized_dataset))
+train_size = int(0.99 * len(tokenized_dataset))
 train_dataset, val_dataset = random_split(tokenized_dataset, [train_size, len(tokenized_dataset) - train_size])
 print(len(train_dataset), len(val_dataset))
+
 '''
 model training
 '''
-training_args = TrainingArguments(output_dir='./results', 
-                                 num_train_epochs=3, 
-                                 learning_rate=3e-6,
-                                 logging_steps=100, 
-                                 save_strategy='epoch',
-                                 evaluation_strategy = 'epoch',
-                                 per_device_train_batch_size=6, 
-                                 per_device_eval_batch_size=6, 
-                                 lr_scheduler_type="cosine",
-                                 warmup_steps= 1000,
-                                 weight_decay=0.01,
-                                 fp16=True,
-                                 gradient_checkpointing=True,
-                                 deepspeed='./config_file/ds_config_sft.json')
+training_args = TrainingArguments(
+    output_dir='./results',          # output directory
+    num_train_epochs=3,              # total number of training epochs
+    per_device_train_batch_size=16,  # batch size per device during training
+    per_device_eval_batch_size=16,   # batch size for evaluation
+    warmup_steps=150,                # number of warmup steps for learning rate scheduler
+    weight_decay=0.01,               # strength of weight decay
+    evaluation_strategy="epoch",     # Evaluation is done at the end of each epoch
+    logging_steps=10,
+    save_strategy='epoch',
+    learning_rate=5e-6,
+    fp16=True,
+    lr_scheduler_type="cosine",
+    gradient_checkpointing=True,
+    deepspeed='./config_file/ds_config_sft.json'
+)
 
-#使用flashattention
-if flash_attn:
-    from flash_attn_patch import replace_llama_attn_with_flash_attn
-    replace_llama_attn_with_flash_attn()
+def my_scheduler(optimizer):
+    return get_cosine_schedule_with_warmup(optimizer, num_warmup_steps=150, num_training_steps=1600)
+
+training_args.lr_scheduler_fn = my_scheduler
 
 model = LlamaForCausalLM.from_pretrained(model_name, use_cache =False).cuda()
 print('model_loaded')
-
 
 
 def the_collate_fn(batch):  
@@ -97,7 +103,3 @@ trainer = Mytrainer(model=model,
                     data_collator=the_collate_fn
                     )
 trainer.train()
-
-'''
-在ChineseLlama2 测试中发现使用flashattention 微调4096长度指令，batch=48,每步能够缩短4s左右
-'''
