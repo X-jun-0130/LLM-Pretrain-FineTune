681c376374b0203ba0133091fc7c3e8deeb29f0b
No_GPT <46883256+X-jun-0130@users.noreply.github.com>
2026-05-03 11:31:45 +0800

    模型堆叠后，使用新的训练方案进行分层学习率微调

--- Model_Sft_Stack.py
+++ Model_Sft_Stack.py
@@ -1,4 +1,4 @@
-# deepspeed --include localhost:0,1,2,3,4,5,6,7 /workspace/Xuxiangjun/LLM-Train/LLM_SFT/Model_Sft_Stack.py
+# deepspeed --include localhost:0,1,2,3,4,5,6,7 /LLM-Train/LLM_SFT/Model_Sft_Stack.py
 import os
 import re
 import torch
